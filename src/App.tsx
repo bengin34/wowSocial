7ba1b81f9bef720eaf0e83453cc636ec1f7e92af
bengin34 <becaglar3434@gmail.com>
2024-02-11 21:16:54 +0100

    Bottom and roots

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import LoginForm from "./_auth/forms/LoginForm";
 import RegisterForm from "./_auth/forms/RegisterForm";
-import { Home } from "./_root/pages";
+import {
+  AllUsers,
+  CreatePost,
+  Explore,
+  Home,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdatePost,
+  UpdateProfile,
+} from "./_root/pages";
 import "./globals.css";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
@@ -20,6 +30,14 @@ const App = () => {
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/allusers" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/updte-post/:id" element={<UpdatePost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/update-profile" element={<UpdateProfile />} />
         </Route>
       </Routes>
 
--- src/components/shared/BottomBar.tsx
+++ src/components/shared/BottomBar.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import { bottombarLinks } from "@/constants";
+import { Link, useLocation } from "react-router-dom";
 
 const BottomBar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { pathname } = useLocation();
 
-export default BottomBar
+  return (
+    <section className="bottom-bar">
+      {bottombarLinks.map((link: INavLink) => {
+        const isActive = pathname === link.route;
+
+        return (
+          <div
+            className={`bottombar-link ${
+              isActive && "bg-primary-600 rounded-[10px]"
+            } flex-center flex-col gap-1 p-2 transition`}
+          >
+            <link.icon />
+            <Link className="flex gap-4 items-center " to={link.route}>
+              <p className="tiny-medium text-light-2">{link.label}</p>
+            </Link>
+          </div>
+        );
+      })}
+    </section>
+  );
+};
+
+export default BottomBar;
