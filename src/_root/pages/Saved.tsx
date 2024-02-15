aeb13a632cf0f5b73247721f91d56b39ba9f540d
bengin34 <becaglar3434@gmail.com>
2024-02-15 18:15:46 +0100

    Saved page

--- src/_root/pages/Saved.tsx
+++ src/_root/pages/Saved.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import GridPostList from "@/components/shared/GridPostList";
+import Loader from "@/components/shared/Loader";
+import { useGetCurrentUser } from "@/lib/react-query/queriesAndMutations";
+import { Models } from "appwrite";
+import { FaRegBookmark } from "react-icons/fa";
 
 const Saved = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { data: currentUser } = useGetCurrentUser();
 
-export default Saved
+  const savePosts = currentUser?.save
+    .map((savePost: Models.Document) => ({
+      ...savePost.post,
+      creator: {
+        imageUrl: currentUser.imageUrl,
+      },
+    }))
+    .reverse();
+
+  return (
+    <div className="saved-container">
+      <div className="flex gap-2 justify-center items-center w-full max-w-5xl">
+        <FaRegBookmark size={20} />
+        <h2 className="h3-bold md:h2-bold textleft w-full">Saved Posts</h2>
+      </div>
+
+      {!currentUser ? (
+        <Loader />
+      ) : (
+        <ul className="w-full flex justify-center max-w-5xl gap-9">
+          {savePosts.length === 0 ? (
+            <p className="text-light-4">No available posts</p>
+          ) : (
+            <GridPostList posts={savePosts} showStats={false} />
+          )}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Saved;
